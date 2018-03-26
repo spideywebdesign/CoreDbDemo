8e6bc1dd510bbbabcbe5aaff2712fbb460632d65
Mark Johnson <mark@wecreateclarity.co.uk>
2018-03-26 16:44:51 +0100

    Add project files.

---
+++ CoreDbDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDbDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
